0c9e84435cc309aea9aa00f676b5faa10efac438
Sonabayim <huseynzade.sonabeyim@gmail.com>
2018-06-13 03:31:32 +0400

    updated choices

--- src/profiles/choices.py
+++ src/profiles/choices.py
@@ -1,18 +1,22 @@
 GENDER_CHOICES = (
-    (1, 'Male'),
-    (2, 'Female'),
+	(1, 'Male'),
+	(2, 'Female'),
  )
 
 BLOOD_GROUP = (
-    (1, 'I'),
-    (2, 'II'),
-    (3, 'III'),
-    (4, 'IV'),
+	(1, 'I +'),
+	(2, 'II +'),
+	(3, 'III +'),
+	(4, 'IV + '),
+	(5, 'I -'),
+	(6, 'II -'),
+	(7, 'III -'),
+	(8, 'IV -'),  
 
 
 )
 
 ILLNESS_CHOICES = (
-    (1, 'Bəli'),
-    (2, 'Xeyr'),
+	(1, 'Bəli'),
+	(2, 'Xeyr'),
  )
